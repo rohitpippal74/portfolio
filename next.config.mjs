429827f696a07fcdd3ca0fc8888c3a1fea544d8b
rohitpippale74 <rohitpippal74@gmail.com>
2025-08-23 16:52:43 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,31 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//    assetPrefix: '/portfolio',
+//   basePath: '/portfolio',
+//   trailingSlash: true,
+// }
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-   assetPrefix: '/portfolio',
-  basePath: '/portfolio',
-  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  // remove basePath & assetPrefix
+  // basePath: '/portfolio',
+  // assetPrefix: '/portfolio',
 }
 
 export default nextConfig
